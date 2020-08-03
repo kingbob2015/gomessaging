2f58fb38567283db6d1b2d9c68f1394db939ac8d
robert.king@radiancetech.com <kingbob2015@hotmail.com>
2020-08-03 14:57:40 -0400

    making lots of progress

--- server/server.go
+++ server/server.go
@@ -1,34 +1,75 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/kingbob2015/gomessaging/proto/messagingpb"
+	"github.com/kingbob2015/gomessaging/server/utils"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
 	messagingpb.UnimplementedMessagingServiceServer
+	//Going to keep the clientList here as a pointer for now and as a named field instead of embedded type
+	//This could go either way but going to keep as pointer in case we want to share clients among multiple servers
+	//Going to keep as named field to keep constructor style in client list utilities
+	clientList *utils.ClientList
 }
 
-// func (*server) RegisterAsClient(context.Context, *RegisterAsClientRequest) (*RegisterAsClientResponse, error) {
+func newServer() *server {
+	return &server{clientList: utils.NewClientList()}
+}
+
+func (s *server) serverValidCheck() error {
+	if s.clientList == nil {
+		return errors.New("The client list of the server is uninitialized")
+	}
+	return nil
+}
+
+func (s *server) RegisterAsClient(ctx context.Context, req *messagingpb.RegisterAsClientRequest) (*messagingpb.RegisterAsClientResponse, error) {
+	err := s.serverValidCheck()
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("An error occurred during server validation: %v", err),
+		)
+	}
+
+	name := req.GetDisplayName()
+	id, err := s.clientList.AddNewClient(name)
+	//improvement here: we should have different error types and return diff codes for each type
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("An error occurred during adding of client: %v", err),
+		)
+	}
+
+	return &messagingpb.RegisterAsClientResponse{
+		UserId: id,
+	}, nil
+}
+
+// func (s *server) OpenReceiveChannel(req *messagingpb.ReceiveChannelRequest, messagingpb.MessagingService_OpenReceiveChannelServer) error {
 
 // }
 
-// func (*server) OpenReceiveChannel(*ReceiveChannelRequest, MessagingService_OpenReceiveChannelServer) error {
+// func (s *server) GetClientList(context.Context, *GetClientListRequest) (*GetClientListResponse, error) {
 
 // }
 
-// func (*server) GetClientList(context.Context, *GetClientListRequest) (*GetClientListResponse, error) {
-
-// }
-
-// func (*server) SendMessage(context.Context, *SendMessageRequest) (*SendMessageResponse, error) {
+// func (s *server) SendMessage(context.Context, *SendMessageRequest) (*SendMessageResponse, error) {
 
 // }
 
@@ -44,7 +85,8 @@ func main() {
 
 	opts := []grpc.ServerOption{}
 	s := grpc.NewServer(opts...)
-	messagingpb.RegisterMessagingServiceServer(s, &server{})
+	serv := newServer()
+	messagingpb.RegisterMessagingServiceServer(s, serv)
 
 	reflection.Register(s)
 
--- server/utils/client_list.go
+++ server/utils/client_list.go
@@ -1,4 +1,90 @@
 package utils
 
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/kingbob2015/gomessaging/proto/messagingpb"
+)
+
+//ClientList is a type for holding a client list. Should always be created via NewClientList()
 type ClientList struct {
+	list []*client
 }
+
+type client struct {
+	id         string
+	name       string
+	connection messagingpb.MessagingService_OpenReceiveChannelServer
+}
+
+//NewClientList is a constructor for the ClientList type
+func NewClientList() *ClientList {
+	s := make([]*client, 0, 0)
+	return &ClientList{list: s}
+}
+
+//GetList returns the list of display names of clients currently registered
+func (c *ClientList) GetList() ([]string, error) {
+	if c.list == nil {
+		return []string{}, errors.New("ClientList list is not initialized")
+	}
+	ret := make([]string, len(c.list))
+	for i, cl := range c.list {
+		ret[i] = cl.name
+	}
+	return ret, nil
+}
+
+//GetUserID takes a user's display name and returns their uuid
+func (c *ClientList) GetUserID(name string) (string, error) {
+	if c.list == nil {
+		return "", errors.New("ClientList list is not initialized")
+	}
+	for _, cl := range c.list {
+		if cl.name == name {
+			return cl.id, nil
+		}
+	}
+	return "", errors.New("User does not exist")
+}
+
+//GetUserName takes a user's id and returns their uuid
+func (c *ClientList) GetUserName(id string) (string, error) {
+	if c.list == nil {
+		return "", errors.New("ClientList list is not initialized")
+	}
+	for _, cl := range c.list {
+		if cl.id == id {
+			return cl.name, nil
+		}
+	}
+	return "", errors.New("User does not exist")
+}
+
+func (c *ClientList) userNameExists(name string) bool {
+	for _, cl := range c.list {
+		if cl.name == name {
+			return true
+		}
+	}
+	return false
+}
+
+//AddNewClient generates a uuid for a client and adds them to the client list and returns the uuid as a string
+func (c *ClientList) AddNewClient(name string) (string, error) {
+	if c.list == nil {
+		return "", errors.New("ClientList list is not initialized")
+	}
+	if c.userNameExists(name) {
+		return "", errors.New("User name is in use")
+	}
+	id := uuid.New().String()
+	c.list = append(c.list, &client{id: id, name: name})
+	return id, nil
+}
+
+//SetClientConn takes a stream that is used to send messages to a client and adds it to a user's record
+// func (c ClientList) SetClientConn(id string, stream *messagingpb.MessagingService_OpenReceiveChannelServer) error {
+// 	return nil
+// }
